522058b0767b25117ff6d33d9a04e258958b08ae
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2024-08-20 17:25:30 +0800

    BUG

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="paypal"></div>
+</template>
+
+<script setup lang="ts">
+import { loadScript } from '@paypal/paypal-js'
+
+loadScript({
+  clientId: 'xxxxx'
+}).then((result: any) => {
+  result?.Buttons()?.render('#paypal')
+})
+</script>
